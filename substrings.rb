75e91e61ff6667371fb1ed53b7ad96d15c8987aa
Enrico Lambino <enricolambino@yahoo.com>
2017-07-29 17:44:47 +0800

    used map instead of collect

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,5 @@
 def substrings(str, dict)
-  instances = dict.collect { |e|  str.downcase.scan(e)}.reject { |e|  e.empty?}
+  instances = dict.map { |e|  str.downcase.scan(e)}.reject { |e|  e.empty?}
   return Hash[instances.map {|row| [row[0], row.length]}]
 end
 
